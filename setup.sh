7cdf239b1773487ffda0d48e1ba345cc0fb7d6c5
Andrei Dascalu <andrei.dascalu@qubiz.com>
2020-11-07 15:11:22 +0200

    setup

--- setup.sh
+++ setup.sh
@@ -25,3 +25,6 @@ apt-get -y install docker-ce docker-ce-cli containerd.io
 
 curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+ulimit -n 65536
+ulimit -u 4096
